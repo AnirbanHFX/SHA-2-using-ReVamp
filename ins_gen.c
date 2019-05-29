ce6d751514f8557e942cfd294f184ee9b6163db0
Anirban Majumder <43907249+HalcyonicFalconX@users.noreply.github.com>
2019-05-29 14:49:59 +0700

    Update ins_gen.c

--- ins_gen.c
+++ ins_gen.c
@@ -17,6 +17,168 @@ void Rotate(int c, FILE *fp) {  // Used for bit shifting
 
 }
 
+int Calculate_carrybits(FILE *fp, int cyc, int m2, int m1) {
+
+   fprintf(fp, "Read %d\n\n", m2); cyc++;
+
+   fprintf(fp, "Apply %d 1 00 000000 ", m2); cyc++;	 // Calculates and stores c1 in bit 63 (leftmost bit is labelled 0)
+   for (int i=0; i<64; i++) {
+      if (i == 63)
+         fprintf(fp, "1 32 ");
+      else if (31-i >=0) 
+         fprintf(fp, "0 %d ", 31-i);
+      else 
+         fprintf(fp, "0 %d ", 64+31-i);
+   }
+   fprintf(fp, "\n\n");
+
+   for (int i=1; i<32; i++) {
+
+      fprintf(fp, "Read %d\n\n", m2); cyc++;
+ 
+      fprintf(fp, "Apply %d 1 11 %d ", m2, 64-i); cyc++; // Calculates ci and stores in bit 63-i
+      for (int j=0; j<64; j++) {
+         if (j == 63-i)
+            fprintf(fp, "1 %d ", 32+i);
+         else if (31-j >=0) 
+            fprintf(fp, "0 %d ", 31-j);
+         else 
+            fprintf(fp, "0 %d ", 64+31-j);
+      }
+      fprintf(fp, "\n\n");
+
+   }
+
+   // Reset left half of line m2
+   // PIR Loads string of 1's
+
+   fprintf(fp, "Apply %d 0 00 000000 ", m2); cyc++;
+   for (int i=0; i<64; i++) {
+      if (63-i >= 32)
+         fprintf(fp, "1 %d ", 63-i);
+      else
+         fprintf(fp, "0 %d ", 63-i);
+   }
+   fprintf(fp, "\n\n");
+
+   fprintf(fp, "Read %d\n\n", m2); cyc++;  // Read c32
+
+   fprintf(fp, "Apply %d 1 01 000000 ", m2); cyc++;  // Copy ~c32 to immediate left
+   for (int i=0; i<64; i++) {
+      if (62-i == 31)
+         fprintf(fp, "1 31 ");
+      else if (62-i >= 0)
+         fprintf(fp, "0 %d ", 62-i);
+      else
+         fprintf(fp, "0 %d ", 62-i+63);
+   }
+   fprintf(fp, "\n\n");   
+
+   fprintf(fp, "Read %d\n\n", m2); cyc++;  // Read ~c31
+
+   fprintf(fp, "Apply %d 1 01 000000 ", m1); cyc++;  // Copy c31 to the corresponding position in m1
+   for (int i=0; i<64; i++) {
+      if (64 + 0-i == 32)
+         fprintf(fp, "1 32 ");
+      else if (0-i >= 0)
+         fprintf(fp, "0 %d ", 0-i);
+      else
+         fprintf(fp, "0 %d ", 0-i+64);
+   }
+   fprintf(fp, "\n\n");   
+
+   // Calculate remaining carry bits
+
+   fprintf(fp, "Read %d\n\n", m1); cyc++;
+
+   fprintf(fp, "Apply %d 1 11 32 ", m1); cyc++;	 // Calculates and stores c1 in bit 63 (leftmost bit is labelled 0)
+   for (int i=0; i<64; i++) {
+      if (i == 63)
+         fprintf(fp, "1 32 ");
+      else if (31-i >= 0) 
+         fprintf(fp, "0 %d ", 31-i);
+      else 
+         fprintf(fp, "0 %d ", 64+31-i);
+   }
+   fprintf(fp, "\n\n");
+   
+
+   for (int i=1; i<31; i++) {  // Does not compute c64
+
+      fprintf(fp, "Read %d\n\n", m1); cyc++;
+ 
+      fprintf(fp, "Apply %d 1 11 %d ", m1, 64-i); cyc++; // Calculates ci and stores in bit 63-i
+      for (int j=0; j<64; j++) {
+         if (j == 63-i)
+            fprintf(fp, "1 %d ", 32+i);
+         else if (31-j >=0) 
+            fprintf(fp, "0 %d ", 31-j);
+         else 
+            fprintf(fp, "0 %d ", 64+31-j);
+      }
+      fprintf(fp, "\n\n");
+
+   }
+
+   // Reset ~c32 in m2
+   // PIR Loads string of 1's
+   fprintf(fp, "Apply %d 0 00 000000 ", m2); cyc++;
+   for (int i=0; i<64; i++) {
+      if (63-i >= 32)
+         fprintf(fp, "1 %d ", 63-i);
+      else
+         fprintf(fp, "0 %d ", 63-i);
+   }
+   fprintf(fp, "\n\n");
+
+   // Reset left half of m1
+   // PIR Loads string of 1's
+   fprintf(fp, "Apply %d 0 00 000000 ", m1); cyc++;
+   for (int i=0; i<64; i++) {
+      if (63-i >= 32)
+         fprintf(fp, "1 %d ", 63-i);
+      else
+         fprintf(fp, "0 %d ", 63-i);
+   }
+   fprintf(fp, "\n\n");
+
+   // Copy c33 c34 ... c63 to m1 in inverted form
+
+   fprintf(fp, "Read %d\n\n", m1); cyc++;
+
+   fprintf(fp, "Apply %d 1 01 000000 ", m1); cyc++;
+   for (int i=0; i<64; i++) {
+      if (31-i >= 0) {
+         if (31 - i == 31)
+            fprintf(fp, "0 %d ", 31-i);
+         else 
+            fprintf(fp, "1 %d ", 31-i);
+      }
+      else 
+         fprintf(fp, "0 %d ", 31-i+64);
+   }
+   fprintf(fp, "\n\n");
+
+   fprintf(fp, "Read %d\n\n", m1); cyc++;  // Read ~c63 ~c62 ... ~c33
+
+   fprintf(fp, "Apply %d 1 01 000000 ", m2); cyc++;   // Copy c63 c62 ... c33 to m2
+   for (int i=0; i<64; i++) {
+      if (63-i >= 32)
+         fprintf(fp, "1 %d ", 63-i);
+      else
+         fprintf(fp, "0 %d ", 63-i);
+   }
+   fprintf(fp, "\n\n");
+
+   // Reset m1
+   // PIR Loads string of 1's
+   fprintf(fp, "Apply %d 0 00 000000 ", m1); cyc++;
+   Rotate(0, fp);
+
+   return cyc;
+
+}
+
 int Load(FILE *fp, int cyc) {
 
    fprintf(fp, "//The following code loads the initial states to the SHA-2 State partition in inverted form\n\n");
@@ -73,6 +235,7 @@ int Ch_EFG(FILE *fp, int cyc, int tar, int mem2, int mem1) {  // Computation mem
    fprintf(fp, "Apply %d 1 01 000000 ", tar); cyc++; // e.f XOR ~e.g
    Rotate(0, fp);
 
+   // PIR Loads string of 1's
    fprintf(fp, "Apply %d 0 00 000000 ", mem2); cyc++; // RESET wl 8, 9
    Rotate(0, fp);
    fprintf(fp, "Apply %d 0 00 000000 ", mem1); cyc++; 
@@ -132,6 +295,7 @@ int Sigma1(FILE *fp, int cyc, int tar, int mem2, int mem1) {  // Computation mem
    fprintf(fp, "Apply %d 1 01 000000 ", tar); cyc++;  // (e14 XOR e18) XOR e41
    Rotate(0, fp);
 
+   // PIR Loads string of 1's
    fprintf(fp, "Apply %d 0 00 000000 ", mem2); cyc++; // RESET wl mem1, mem2
    Rotate(0, fp);
    fprintf(fp, "Apply %d 0 00 000000 ", mem1); cyc++; 
@@ -191,6 +355,7 @@ int Sigma0(FILE *fp, int cyc, int tar, int mem2, int mem1) {  // Computation mem
    fprintf(fp, "Apply %d 1 01 000000 ", tar); cyc++;  // (a28 XOR a34) XOR a39
    Rotate(0, fp);
 
+   // PIR Loads string of 1's
    fprintf(fp, "Apply %d 0 00 000000 ", mem2); cyc++; // RESET wl mem1, mem2
    Rotate(0, fp);
    fprintf(fp, "Apply %d 0 00 000000 ", mem1); cyc++; 
@@ -272,6 +437,7 @@ int Maj(FILE *fp, int cyc, int tar, int m3, int m2, int m1) {
    fprintf(fp, "Apply %d 1 01 000000 ", tar); cyc++; // (a.b XOR a.c) XOR (b.c)
    Rotate(0, fp);
 
+   // PIR Loads string of 1's
    fprintf(fp, "Apply %d 0 00 000000 ", m3); cyc++;  // RESET wl m3, m2
    Rotate(0, fp);
    fprintf(fp, "Apply %d 0 00 000000 ", m2); cyc++;  
@@ -281,13 +447,24 @@ int Maj(FILE *fp, int cyc, int tar, int m3, int m2, int m1) {
 
 }
 
-int Sum(FILE *fp, int cyc, int a, int b, int m1, int m2, int m3, int m4) {
+int Sum(FILE *fp, int cyc, int a, int b, int m1, int m2, int m3, int m4) {   // Computation memory NOT PREVIOUSLY CLEARED
+						                             // Stored A ++ B in m4, ++ is sum
 
    // Assuming A and B are initially non-inverted
 
-   fprintf(fp, "Read %d\n\n", a); cyc++;             // Read A
+   if (a >= 0) {
 
-   fprintf(fp, "Apply %d 1 01 000000 ", m1); cyc++   // Write ~A
+      fprintf(fp, "Read %d\n\n", a); cyc++;             // Read A
+
+      fprintf(fp, "Apply %d 1 01 000000 ", m1); cyc++;   // Write ~A
+
+   }
+
+   else {
+      // PIR Loads A
+      fprintf(fp, "Apply %d 0 01 000000 ", m1); cyc++;  // Write ~A
+
+   }
 
    fprintf(fp, "Read %d\n\n", m1); cyc++;            // Read ~A
 
@@ -298,14 +475,29 @@ int Sum(FILE *fp, int cyc, int a, int b, int m1, int m2, int m3, int m4) {
    fprintf(fp, "Apply %d 1 01 000000 ", m2); cyc++;  // Write A
    Rotate(0, fp);
 
-   fprintf(fp, "Read %d\n\n", b); cyc++;             // Read B
+   if (b >= 0) {
 
-   fprintf(fp, "Apply %d 1 00 000000 ", m4); cyc++;  // A.~B
-   Rotate(0, fp);
-   fprintf(fp, "Apply %d 1 00 000000 ", m3); cyc++;  // A.~B
-   Rotate(0, fp);
-   fprintf(fp, "Apply %d 1 01 000000 ", m2); cyc++;  // A + ~B
-   Rotate(0, fp); 
+      fprintf(fp, "Read %d\n\n", b); cyc++;             // Read B
+
+      fprintf(fp, "Apply %d 1 00 000000 ", m4); cyc++;  // A.~B
+      Rotate(0, fp);
+      fprintf(fp, "Apply %d 1 00 000000 ", m3); cyc++;  // A.~B
+      Rotate(0, fp);
+      fprintf(fp, "Apply %d 1 01 000000 ", m2); cyc++;  // A + ~B
+      Rotate(0, fp); 
+
+   }
+    
+   else {
+      // PIR Loads B
+      fprintf(fp, "Apply %d 0 00 000000 ", m4); cyc++;  // A.~B
+      Rotate(0, fp);
+      fprintf(fp, "Apply %d 0 00 000000 ", m3); cyc++;  // A.~B
+      Rotate(0, fp);
+      fprintf(fp, "Apply %d 0 01 000000 ", m2); cyc++;  // A + ~B
+      Rotate(0, fp); 
+ 
+   }
 
    fprintf(fp, "Read %d\n\n", m2); cyc++;            // Read A + ~B  
 
@@ -314,13 +506,128 @@ int Sum(FILE *fp, int cyc, int a, int b, int m1, int m2, int m3, int m4) {
    fprintf(fp, "Apply %d 1 01 000000 ", m3); cyc++;  // A XOR B
    Rotate(0, fp);
 
+   // PIR Loads string of 1's
    fprintf(fp, "Apply %d 0 00 000000 ", m2); cyc++;  // RESET m1, m2
    Rotate(0, fp);
    fprintf(fp, "Apply %d 0 00 000000 ", m1); cyc++;
-   Rotate(0, fp);    
+   Rotate(0, fp);
+
+   // Copy A in non_inverted form, half to m2 and half to m1
+
+   if (a >= 0) {
+
+      fprintf(fp, "Read %d\n\n", a); cyc++;             // Read A
+
+      fprintf(fp, "Apply %d 1 01 000000 ", m1); cyc++;   // Write ~A
+      Rotate(0, fp);   
+
+   }   
+
+   else {
+
+      // PIR Loads A
+      fprintf(fp, "Apply %d 0 01 000000 ", m1); cyc++;   // Write ~A
+      Rotate(0, fp);
+
+   }
+
+   fprintf(fp, "Read %d\n\n", m1); cyc++;            // Read ~A
+
+   // PIR Loads String of 1's
+   fprintf(fp, "Apply %d 0 00 000000 ", m1); cyc++;  // Clear m1
+   Rotate(0, fp);
+
+   fprintf(fp, "Apply %d 1 01 000000 ", m2); cyc++;  // Copy a31 a30 a29 ... a0 to m2
+   for(int i=0; i<32; i++) {
+      fprintf(fp, "0 %d ", 63-i);
+   }
+   for(int i=0; i<32; i++) {
+      fprintf(fp, "1 %d ", 31-i);
+   }
+   fprintf(fp, "\n\n");
+
+   fprintf(fp, "Apply %d 1 01 000000 ", m1); cyc++;  // Copy 0 a62 a61 ... a32 to m1
+   for(int i=0; i<32; i++) {
+      fprintf(fp, "0 %d ", 31-i);
+   }
+
+   fprintf(fp, "0 63 ");
+
+   for(int i=1; i<32; i++) {
+      fprintf(fp, "1 %d ", 63-i);
+   }
+   fprintf(fp, "\n\n");   
+
+
+   // Copy B in inverted form, half to m2 and half to m1
+   
+   if (b >= 0) {
+
+      fprintf(fp, "Read %d\n\n", b); cyc++;             // Read B
+
+      fprintf(fp, "Apply %d 1 01 000000 ", m2); cyc++;  // Copy ~b31 ~b30 ~b29 ... ~b0 to m2
+      for(int i=0; i<32; i++) {
+         fprintf(fp, "1 %d ", 31-i);
+      }
+      for(int i=0; i<32; i++) {
+         fprintf(fp, "0 %d ", 63-i);
+      }
+      fprintf(fp, "\n\n");
+
+      fprintf(fp, "Apply %d 1 01 000000 ", m1); cyc++;  // Copy ~b63 ~b62 ~b61 ... ~b32 to m1
+      for(int i=0; i<32; i++) {
+         fprintf(fp, "1 %d ", 63-i);
+      }
+      for(int i=0; i<32; i++) {
+         fprintf(fp, "0 %d ", 31-i);
+      }
+      fprintf(fp, "\n\n");   
+
+   }
+
+   else {
+
+      // PIR Loads B
+      fprintf(fp, "Apply %d 0 01 000000 ", m2); cyc++;  // Copy ~b31 ~b30 ~b29 ... ~b0 to m2
+      for(int i=0; i<32; i++) {
+         fprintf(fp, "1 %d ", 31-i);
+      }
+      for(int i=0; i<32; i++) {
+         fprintf(fp, "0 %d ", 63-i);
+      }
+      fprintf(fp, "\n\n");
+
+      fprintf(fp, "Apply %d 0 01 000000 ", m1); cyc++;  // Copy ~b63 ~b62 ~b61 ... ~b32 to m1
+      for(int i=0; i<32; i++) {
+         fprintf(fp, "1 %d ", 63-i);
+      }
+      for(int i=0; i<32; i++) {
+         fprintf(fp, "0 %d ", 31-i);
+      }
+      fprintf(fp, "\n\n");   
+
+   }
+
+   cyc = Calculate_carrybits(fp, cyc, m2, m1);    // Line m2 contains (Carry >>> 1)
+                                                  // Carry bits need to be left rotated by 1
+   fprintf(fp, "Read %d\n\n", m2); cyc++;         // Read (Carry >>> 1)
+
+   fprintf(fp, "Apply %d 1 00 000000 ", m4); cyc++; // (A XOR B).~C
+   Rotate(63, fp);                                  // Is equivalent to a 1 bit leftrotate
+
+   fprintf(fp, "Apply %d 1 01 000000 ", m3); cyc++; // (A XOR B) + ~C
+   Rotate(63, fp);
+
+   fprintf(fp, "Read %d\n\n", m3); cyc++;         // Read (A XOR B) + ~C 
+
+   fprintf(fp, "Apply %d 1 01 000000 ", m4); cyc++; // (A XOR B) XOR C
+   Rotate(0, fp);         
+
+   return cyc;
 
 }
 
+//int TestSum(FILE *, int); 
 
 int main() {
 
@@ -330,8 +637,9 @@ int main() {
 
    int cyc = 0;
 
-   cyc = Load(fp, cyc);
+   //cyc = TestSum(fp, cyc);
 
    return 0;
 
 }
+
